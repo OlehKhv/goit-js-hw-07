24483e087cf9ed89ad7011686a82b534a225f4a0
OlehKhv <hvostik.oleg@ukr.net>
2023-07-09 17:23:16 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+let instance;
+
+addMarkup(galleryItems);
+
+listEl.addEventListener('click', onClick);
+window.addEventListener('keydown', closeModal);
+
+function onClick(e) {
+    e.preventDefault();
+
+    if (!e.target.classList.contains('gallery__image')) {
+        return;
+    }
+
+    instance = basicLightbox.create(`
+    <img src=${e.target.dataset.source} width="1280">
+`);
+
+    instance.show();
+}
+
+function addMarkup(arrGallery) {
+    const markup = arrGallery
+        .map(
+            ({ preview, original, description }) =>
+                `<li class="gallery__item">
+                    <a class="gallery__link" href=${original}>
+                        <img
+                            class="gallery__image"
+                            src=${preview}
+                            data-source=${original}
+                            alt='${description}'
+                        />
+                    </a>
+                </li>`
+        )
+        .join('');
+    listEl.innerHTML = markup;
+}
+
+function closeModal(e) {
+    if (e.code === 'Escape') instance.close();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+
+addMarkup(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery__link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
+listEl.addEventListener('click', onClick);
+
+function addMarkup(arrGallery) {
+    const markup = arrGallery
+        .map(
+            ({ preview, original, description }) =>
+                `<li class="gallery__item">
+                    <a class="gallery__link" href=${original}>
+                        <img
+                            class="gallery__image"
+                            src=${preview}
+                            alt='${description}'
+                        />
+                    </a>
+                </li>`
+        )
+        .join('');
+    listEl.innerHTML = markup;
+}
+
+function onClick(e) {
+    e.preventDefault();
+}
